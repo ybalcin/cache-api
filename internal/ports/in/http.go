def1ed60eb7f17c5750881a2ebb6e63e34536594
Yigitcan Balcin <ybalcin1@gmail.com>
2021-11-20 00:31:16 +0300

    log request

--- internal/ports/in/http.go
+++ internal/ports/in/http.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ybalcin/cache-api/internal/common"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -22,6 +23,16 @@ type (
 	}
 )
 
+const (
+	contentType     = "Content-Type"
+	applicationJson = "application/json"
+	requestId       = "X-Request-ID"
+)
+
+var (
+	logger = log.New(os.Stdout, "http: ", log.LstdFlags)
+)
+
 // NewHttpServer initializes a new http server input port
 func NewHttpServer() *httpServer {
 	app := application.New()
@@ -31,17 +42,24 @@ func NewHttpServer() *httpServer {
 
 // ServeHTTP middleware
 func (h Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// log incoming request as stdout
+	defer logRequest(req)
+
 	if req.Method != h.Method {
 		http.Error(w, "", http.StatusMethodNotAllowed)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
+
+	w.Header().Set(contentType, applicationJson)
 
 	err := h.H(w, req)
+	if e, ok := recover().(error); ok && e != nil {
+		err = e
+	}
 	if err != nil {
 		switch e := err.(type) {
 		case common.Error:
-			log.Printf("HTTP status: %d - Message: %s", e.Status(), e.Error())
+			logger.Printf("HTTP status: %d - Message: %s", e.Status(), e.Error())
 			http.Error(w, "", e.Status())
 		default:
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -85,4 +103,12 @@ func (s *httpServer) GetValueHandler(w http.ResponseWriter, req *http.Request) e
 func (s *httpServer) FlushHandler(w http.ResponseWriter, req *http.Request) error {
 	s.Application.CacheService.ClearAll()
 	return nil
+}
+
+func logRequest(req *http.Request) {
+	requestID := req.Header.Get(requestId)
+	if requestID == "" {
+		requestID = "unknown"
+	}
+	logger.Println(requestID, req.Method, req.URL.Path, req.RemoteAddr, req.UserAgent())
 }
